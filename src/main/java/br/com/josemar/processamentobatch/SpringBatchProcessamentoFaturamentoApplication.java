2590f11f48fbe78bf8fb2d20e09fdc950298b2f2
Josemar Junior <junioraguiar255@gmail.com>
2026-07-05 02:08:09 -0300

    chore: inicializa projeto Spring batch de faturamento

---
+++ src/main/java/br/com/josemar/processamentobatch/SpringBatchProcessamentoFaturamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.josemar.processamentobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchProcessamentoFaturamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchProcessamentoFaturamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/josemar/processamentobatch/SpringBatchProcessamentoFaturamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.josemar.processamentobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchProcessamentoFaturamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
